6a3e16fd3a3672cbbf1b481ce57f3570e572e26b
akib-raihan-sazim <akib.raihan@sazim.io>
2024-07-10 15:32:37 +0600

    Add task (#2)
    * Create a new React App using vite.
    Setup Typescript for the project
    Install and configure Mantine for styling.
    
    * chore(FTDA): Remove unnecessary files and folders.
    
    * feat(FTDA): Added Button for adding tasks.
    feat(FTDA): Modal is created for the TO-DO form.
    feat(FTDA): Save task on localStorage.
    
    * fix(FTDA): Date input is fixed
    chore(FTDA): Removed unnecessary imports
    
    * dummy commit for rebasing
    
    * chore(FTDA): File structure are changed according to conventions.
    chore(FTDA): Remove unnecessary lines.
    chore(FTDA): Imports are done according to conventions
    
    * chore(FTDA): Updated the access of TaskProvider context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { MantineProvider } from '@mantine/core'
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/dates/styles.css";
+
+import "./index.css";
+import { TasksProvider } from "./contexts/TasksContext";
+import HomePageContainer from "./Pages/HomePage/Containers/HomePageContainer";
 
 function App() {
-  
-
   return (
-    <MantineProvider>
-      App
+    <MantineProvider defaultColorScheme="dark">
+      <TasksProvider>
+        <HomePageContainer />
+      </TasksProvider>
     </MantineProvider>
-  )
+  );
 }
-
-export default App
+export default App;
