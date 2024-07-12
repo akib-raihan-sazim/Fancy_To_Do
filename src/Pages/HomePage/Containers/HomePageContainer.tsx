55f901a7e7e5fdbaad99e4acbddd25b087a5490b
akib-raihan-sazim <akib.raihan@sazim.io>
2024-07-12 16:28:08 +0600

    feat(FTDA): Functionality to undo last action. (#11)
    chore(FTDA): Added some extra css and icons for UI.

--- src/Pages/HomePage/Containers/HomePageContainer.tsx
+++ src/Pages/HomePage/Containers/HomePageContainer.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
-import { Button, Container } from "@mantine/core";
+import { Button, Container, Group } from "@mantine/core";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 
 import ToDoForm from "../Components/ToDoForm";
 import TaskList from "../Components/TaskList";
+import { useTasks } from "../../../contexts/TasksContext";
 
 const HomePageContainer = () => {
   const [opened, setOpened] = useState(false);
+  const { undoLastAction } = useTasks();
 
   const handleToDoForm = () => {
     setOpened(true);
@@ -22,8 +25,19 @@ const HomePageContainer = () => {
       >
         Add Task
       </Button>
+      <Group className="undo-redo-space">
+        <Button
+          className="undo-btn"
+          variant="gradient"
+          gradient={{ from: "red", to: "orange", deg: 90 }}
+          onClick={undoLastAction}
+        >
+          <FaArrowAltCircleLeft className="left-icon" />
+          UNDO
+        </Button>
+      </Group>
       <ToDoForm opened={opened} setOpened={setOpened} />
-      <TaskList/>
+      <TaskList />
     </Container>
   );
 };
