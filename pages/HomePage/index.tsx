383a11b3066150f80d4f53d68140eacd763a9583
Akib Raihan <akib.raihan@sazim.io>
2024-07-17 18:02:37 +0600

    feat(FTDA): Migrate the app from vite -> Next.js

---
+++ pages/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+
+import HomePageContainer from "@/modules/HomePage/containers/HomePageContainer";
+
+const HomePage = () => {
+  return <HomePageContainer />;
+};
+
+export default HomePage;
