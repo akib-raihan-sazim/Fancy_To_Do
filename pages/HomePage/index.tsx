76ce1fce383ead48de44295b1dc171440096f0de
Akib Raihan <akib.raihan@sazim.io>
2024-07-25 11:45:16 +0600

    fix(FTDA): Imported the mantine css on _app.tsx. fix(FTDA): TaskList.tsx files is fixed.

--- pages/HomePage/index.tsx
+++ pages/HomePage/index.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "@mantine/core/styles.css";
-import "@mantine/dates/styles.css";
 
 import HomePageContainer from "@/modules/HomePage/containers/HomePageContainer";
 
