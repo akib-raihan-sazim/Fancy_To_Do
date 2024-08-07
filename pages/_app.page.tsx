c16286940eee4d846b769c7b2859500a0815ee2b
akib-raihan-sazim <akib.raihan@sazim.io>
2024-08-07 13:38:59 +0600

    feat(FTDA): Setup redux toolkit and added redux provider on the root. (#19)
    chore(FTDA): Integreated the base URL on the apiSlice.

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -3,15 +3,19 @@ import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
 import { MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 
 import { TasksProvider } from "@/modules/HomePage/components/TaskContext/TaskContext";
+import { store } from "@/shared/redux/store";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider defaultColorScheme="dark">
-      <TasksProvider>
-        <Component {...pageProps} />
-      </TasksProvider>
+      <Provider store = {store}>
+        <TasksProvider>
+          <Component {...pageProps} />
+        </TasksProvider>
+      </Provider>
     </MantineProvider>
   );
 }
